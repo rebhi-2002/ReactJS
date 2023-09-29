76502dd79c43271e66aed9da1033a0af8c20ab99
Rebhe Ibrahim <rebheibrahim@gmail.com>
2023-09-29 12:06:44 +0300

    🎥 Update Movies Project with Redux

---
+++ Redux/Movies Final - With Redux/02-Movies Final/src/redux/store/store.js
@@ -0,0 +1,10 @@
+import { legacy_createStore as createStore, applyMiddleware } from "redux";
+import { moviesReducer } from "../reducer/movieReducer";
+
+// import { composeWithDevTools } from "redux-devtools-extension";
+import { composeWithDevTools } from "@redux-devtools/extension";
+
+import thunk from "redux-thunk";
+
+// 1-create store
+export const store = createStore(moviesReducer, applyMiddleware(thunk));
